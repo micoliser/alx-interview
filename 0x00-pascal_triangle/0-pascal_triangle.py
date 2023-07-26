f314493a82fed8b7bb99d3fc122fb84816309f39
micoliser <micoliser@gmail.com>
2023-07-26 11:33:38 +0100

    Write function for pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This module contains a pascals triangle function """
+
+
+def pascal_triangle(n):
+    """ 
+        returns a list of lists of integers reprsenting
+        the Pascal's triangle of n
+    """
+
+    triangle_list = []
+
+    if n >= 1:
+        triangle_list.append([1])
+    if n >= 2:
+        triangle_list.append([1, 1])
+
+    if not n > 2:
+        return triangle_list
+
+    for i in range(2, n):
+        new_tri = [1]
+        prev_tri = triangle_list[i - 1]
+        for j in range(0, len(prev_tri) - 1):
+            new_tri.append(prev_tri[j] + prev_tri[j + 1])
+        new_tri.append(1)
+        triangle_list.append(new_tri)
+
+    return triangle_list
