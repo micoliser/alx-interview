f87f1f7737180c81bce1ca4d1bf40d9ff5917b95
micoliser <micoliser@gmail.com>
2023-08-20 21:00:55 -0700

    Parsed Logs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-"""Log parsing"""
-import sys
+"""
+    Log parsing This module parses a print_stats (or log file)
+    and returns data from the lines
+"""
 import re
 
 
-def print_stats(size, status_codes):
-    """ prints the statistics """
+def print_stats(message: dict) -> None:
+    """ Logs the status to the screen """
 
-    print("File size: {}".format(size))
-    for key in sorted(status_codes.keys()):
-        if status_codes[key] != 0:
-            print("{}: {}".format(key, status_codes[key]))
+    for key, value in message.items():
+        if value != 0 or key == "File size":
+            print(f"{key}: {value}", flush=True)
 
 
-pattern = re.compile(
-    r"([0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9])\ \-\ \[([0-9]{4}\-[0-1]{1}[0-9]{1}\-[0-3]{1}[0-9]{1}\ [0-2]{1}[0-9]{1}\:[0-5]{1}[0-9]{1}\:[0-5]{1}[0-9]{1}\.[0-9]{6})\]\ (\"GET\ \/projects\/260\ HTTP\/1\.1\")\ (200|301|400|401|403|404|405|500)\ ([1-9][0-9]*)")
-
-size = 0
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
+message = {
+    "File size": 0, 200: 0, 301: 0, 400: 0, 401: 0,
+    403: 0, 404: 0, 405: 0, 500: 0}
 counter = 0
+regEX = re.compile(r"""([0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?
+[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]|[^\ ]*[^\ ])\ ?\-\ ?\[([0-9]{4}\-[0-1]{1}
+[0-9]{1}\-[0-3]{1}[0-9]{1}\ [0-2]{1}[0-9]{1}\:[0-5]{1}[0-9]{1}\:
+[0-5]{1}[0-9]{1}\.[0-9]{6})\]\ (\"GET\ \/projects\/260\ HTTP\/1\.1\")
+\ ([^\ ]*[^\ ])\ ([1-9][0-9]*)""", re.X)
+# \ (200|301|400|401|403|404|405|500|*)\ ([1-9][0-9]*)""", re.X)
+# changed to allow anything
 
 try:
-    for line in sys.stdin:
-        if re.match(pattern, line) is None:
-            continue
-        line = line.strip()
-        line = line.split()
-        size += int(line[-1])
-        try:
-            status_codes[line[-2]] += 1
-        except KeyError:
-            pass
-        counter += 1
+    while True:
+        line = input()
         if counter == 10:
-            print_stats(size, status_codes)
+            print_stats(message)
             counter = 0
-except KeyboardInterrupt:
-    print_stats(size, status_codes)
-    raise
+        valid = re.match(regEX, line)
+        counter += 1
+        if not valid:
+            continue
+        valid = regEX.search(line)
+        res = list(valid.groups())
+        message["File size"] += int(res[-1])
+        try:
+            message[int(res[-2])] += 1
+        except ValueError:
+            pass
+except (KeyboardInterrupt, EOFError):
+    print_stats(message)
