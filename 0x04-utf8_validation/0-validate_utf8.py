08b506d3019167f12efd56790a5497b29e841e19
micoliser <micoliser@gmail.com>
2023-08-24 00:41:05 +0100

    fixing

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,32 +10,44 @@
 def check_one_byte(data, index):
     """ checks one byte of data if it is a valid utf-8 """
 
-    if data[index].startswith('0'):
-        return (True, index + 1)
+    try:
+        if data[index].startswith('0'):
+            return (True, index + 1)
+    except IndexError:
+        pass
     return (False, )
 
 
 def check_two_bytes(data, index):
     """ checks two bytes of data if it is a valid utf-8 """
 
-    if data[index].startswith('110'):
-        return check_one_byte(data, index + 1)
+    try:
+        if data[index].startswith('110'):
+            return check_one_byte(data, index + 1)
+    except IndexError:
+        pass
     return (False, )
 
 
 def check_three_bytes(data, index):
     """ checks 3 bytes of data if it is a valid utf-8 """
 
-    if data[index].startswith('1110'):
-        return check_two_bytes(data, index + 1)
+    try:
+        if data[index].startswith('1110'):
+            return check_two_bytes(data, index + 1)
+    except IndexError:
+        pass
     return (False, )
 
 
 def check_four_bytes(data, index):
     """ checks 4 bytes of data if is a valid utf-8 """
 
-    if data[index].startswith('11110'):
-        return check_three_bytes(data, index + 1)
+    try:
+        if data[index].startswith('11110'):
+            return check_three_bytes(data, index + 1)
+    except IndexError:
+        pass
     return (False, )
 
 
@@ -56,14 +68,17 @@ def check_data(data, index):
     return (False, )
 
 
-def validUTF8(data):
+def validUTF8(bin_data):
     """ checks a data if it is valid utf-8 """
 
+    if len(bin_data) == 0:
+        return
+
     i = ('', 0)
-    bin_data = [bin(ch)[2:].zfill(8) for ch in data]
+    # bin_data = [bin(ch)[2:].zfill(8) for ch in data]
     while True:
         i = check_data(bin_data, i[1])
         if not i[0]:
             return False
-        elif i[1] >= len(data):
+        elif i[1] >= len(bin_data):
             return True
