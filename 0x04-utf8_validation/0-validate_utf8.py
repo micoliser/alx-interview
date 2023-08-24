37598fdb7a7b042002c112f92da8c2c25a285f0e
micoliser <micoliser@gmail.com>
2023-08-24 02:49:17 +0100

    Fixed the errors

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,69 +1,34 @@
 #!/usr/bin/python3
 """UTF-8 Validation"""
 
-# data = [229, 65, 127, 256]
-# print([bin(ch)[2:].zfill(8) for ch in data])
-# data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-# print([bin(ch)[2:].zfill(8) for ch in data])
 
+def check_next_bytes(data, index, n):
+    """ checks next n bytes of data if it is a valid utf-8 """
 
-def check_one_byte(data, index):
-    """ checks one byte of data if it is a valid utf-8 """
-
-    try:
-        if data[index].startswith('0'):
-            return (True, index + 1)
-    except IndexError:
-        pass
-    return (False, )
-
-
-def check_two_bytes(data, index):
-    """ checks two bytes of data if it is a valid utf-8 """
-
-    try:
-        if data[index].startswith('110'):
-            return check_one_byte(data, index + 1)
-    except IndexError:
-        pass
-    return (False, )
-
-
-def check_three_bytes(data, index):
-    """ checks 3 bytes of data if it is a valid utf-8 """
-
-    try:
-        if data[index].startswith('1110'):
-            return check_two_bytes(data, index + 1)
-    except IndexError:
-        pass
-    return (False, )
-
-
-def check_four_bytes(data, index):
-    """ checks 4 bytes of data if is a valid utf-8 """
-
-    try:
-        if data[index].startswith('11110'):
-            return check_three_bytes(data, index + 1)
-    except IndexError:
-        pass
-    return (False, )
+    for i in range(index, index + n):
+        try:
+            if not data[i].startswith('10'):
+                return (False, )
+        except IndexError:
+            return (False, )
+    return (True, i + 1)
 
 
 def check_data(data, index):
     """ checks a data if it as valid utf-8 """
 
+    if data[index].startswith('0'):
+        return (True, index + 1)
+
     dt = {
-        '0': check_one_byte,
-        '110': check_two_bytes,
-        '1110': check_three_bytes,
-        '11110': check_four_bytes
+        '110': 1,
+        '1110': 2,
+        '11110': 3
     }
 
-    for dta, function in dt.items():
+    for dta, next_bytes in dt.items():
         if data[index].startswith(dta):
-            return function(data, index)
+            return check_next_bytes(data, index + 1, next_bytes)
 
     return (False, )
 
