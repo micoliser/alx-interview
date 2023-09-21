58c618f617749de37117e6abf44f32370380f522
micoliser <micoliser@gmail.com>
2023-09-21 11:26:55 +0100

    Task done

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Making Change """
+
+
+def makeChange(coins, total):
+    """ Given a pile of coins of different values, determine the fewest
+        number of coins needed to meet a given amount total """
+
+    if total <= 0:
+        return 0
+
+    for i in range(len(coins)):
+        count = 0
+        amount = 0
+        for coin in reversed(sorted(coins[i:])):
+            while amount + coin <= total:
+                amount += coin
+                count += 1
+            if amount == total:
+                return count
+
+    return -1
+
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
